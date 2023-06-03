cd229913a10308262b17a417bdf93830d87a61fc
moojing <starts789@gmail.com>
2023-06-03 19:50:26 +0800

    feat: deploy contract successfully

--- section3/CompoundPractice/script/init.sol
+++ section3/CompoundPractice/script/init.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "../contracts/SimpleSwap.sol";
 
 import { Comptroller } from "compound-protocol/contracts/Comptroller.sol";
 import { WhitePaperInterestRateModel } from "compound-protocol/contracts/WhitePaperInterestRateModel.sol";
+import { CErc20Delegate } from "compound-protocol/contracts/CErc20Delegate.sol";
+import { CErc20Delegator } from "compound-protocol/contracts/CErc20Delegator.sol";
 
 contract MyScript is Script {
     function run() external {
@@ -22,8 +23,29 @@ contract MyScript is Script {
         Comptroller comptroller = new Comptroller();
         WhitePaperInterestRateModel rateModel = new WhitePaperInterestRateModel(0,0);
 
-        // SimpleSwap swap = new SimpleSwap(address(token1), address(token2));
-
+        // deply CErc20Delegator 
+            // constructor(address underlying_,
+            //     ComptrollerInterface comptroller_,
+            //     InterestRateModel interestRateModel_,
+            //     uint initialExchangeRateMantissa_,
+            //     string memory name_,
+            //     string memory symbol_,
+            //     uint8 decimals_,
+            //     address payable admin_,
+            //     address implementation_,
+            //     bytes memory becomeImplementationData) {
+        CErc20Delegator delegator = new CErc20Delegator(
+            address(underlyingToken),
+            comptroller,
+            rateModel,
+            0,
+            "cToken",
+            "cToken",
+            18,
+            payable(address(this)),
+            address(0),
+            ""
+        );
         vm.stopBroadcast();
     }
 }
