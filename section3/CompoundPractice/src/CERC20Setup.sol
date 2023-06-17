d6e0d598286fbc50d9fa88755d180eea0407ea93
moojing <starts789@gmail.com>
2023-06-18 02:15:57 +0800

    feat: finish borrow settings.

--- section3/CompoundPractice/src/CERC20Setup.sol
+++ section3/CompoundPractice/src/CERC20Setup.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-import {CompScript} from "./comp.sol";
 import {Comptroller} from "compound-protocol/contracts/Comptroller.sol";
 import {WhitePaperInterestRateModel} from "compound-protocol/contracts/WhitePaperInterestRateModel.sol";
 import {CErc20Delegate} from "compound-protocol/contracts/CErc20Delegate.sol";
@@ -13,29 +13,6 @@ import {CErc20Delegator} from "compound-protocol/contracts/CErc20Delegator.sol";
 import {Unitroller} from "compound-protocol/contracts/Unitroller.sol";
 import {InterestRateModel} from "compound-protocol/contracts/InterestRateModel.sol";
 import {SimplePriceOracle} from "compound-protocol/contracts/SimplePriceOracle.sol";
-import {Comp} from "compound-protocol/contracts/Governance/Comp.sol";
-import {GovernorBravoDelegate} from "../contract/BravoDelegateCustom.sol";
-import {GovernorBravoDelegator} from "compound-protocol/contracts/Governance/GovernorBravoDelegator.sol";
-import {GovernorAlpha} from "compound-protocol/contracts/Governance/GovernorAlpha.sol";
-
-contract UnderlyingToken is ERC20 {
-    constructor(
-        string memory name_,
-        string memory symbol_
-    ) ERC20(name_, symbol_) {}
-
-    function mint(uint256 amount) public {
-        _mint(msg.sender, amount);
-    }
-}
-
-contract UnderlyingTokenA is UnderlyingToken {
-    constructor() UnderlyingToken("underlying token A", "UDT-A") {}
-}
-
-contract UnderlyingTokenB is UnderlyingToken {
-    constructor() UnderlyingToken("underlying token B", "UDT-B") {}
-}
 
 contract CERC20Setup {
     uint256 deployerPrivateKey;
@@ -43,97 +20,100 @@ contract CERC20Setup {
     Comptroller comptroller;
     Comptroller uniTrollerProxy;
     WhitePaperInterestRateModel rateModel;
+    SimplePriceOracle simpleOracle;
     Unitroller unitroller;
 
-    CErc20Delegator cErc20DelegatorA;
-    CErc20Delegate cErc20delegateA;
-    UnderlyingTokenA underlyingTokenA;
+    CErc20Delegator CErc20DelegatorUSDC;
+    CErc20Delegate CErc20delegateA;
+    // UnderlyingTokenA underlyingTokenA;
 
-    CErc20Delegator cErc20DelegatorB;
-    CErc20Delegate cErc20delegateB;
-    UnderlyingTokenB underlyingTokenB;
-    SimplePriceOracle simpleOracle;
+    CErc20Delegator CErc20DelegatorUNI;
+    CErc20Delegate CErc20delegateB;
+    // UnderlyingTokenB underlyingTokenB;
 
     IERC20 public USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    IERC20 public DAI = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
-    CErc20 public cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
-    CErc20 public cDAI = CErc20(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
+    IERC20 public UNI = IERC20(0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984);
+
+    // CErc20 public cUSDC = CErc20(0x39AA39c021dfbaE8faC545936693aC917d5E7563);
+    // CErc20 public cUNI = CErc20(0x5d3a536E4D6DbD6114cc1Ead35777bAB948E3643);
 
     function preDeploy() public {
         comptroller = new Comptroller();
         rateModel = new WhitePaperInterestRateModel(0, 0);
-    }
-
-    function deployComptroller() public {
-        // deply CErc20Delegator
-        //(    address underlying_,
-        //     ComptrollerInterface comptroller_,
-        //     InterestRateModel interestRateModel_,
-        //     uint initialExchangeRateMantissa_,
-        //     string memory name_,
-        //     string memory symbol_,
-        //     uint8 decimals_,
-        //     address payable admin_,
-        //     address implementation_ )
-        cErc20DelegatorA = new CErc20Delegator(
-            address(underlyingTokenA),
-            Comptroller(address(unitroller)),
-            InterestRateModel(rateModel),
-            1e18,
-            "cToken A",
-            "cTokenA",
-            18,
-            payable(address(this)),
-            address(cErc20delegateA),
-            "0x0"
-        );
-        cErc20DelegatorB = new CErc20Delegator(
-            address(underlyingTokenB),
-            Comptroller(address(unitroller)),
-            InterestRateModel(rateModel),
-            1e18,
-            "cToken B",
-            "cTokenB",
-            18,
-            payable(address(this)),
-            address(cErc20delegateB),
-            "0x0"
-        );
-
-        console.log("delegatorA", address(cErc20DelegatorA));
-        console.log("delegatorB", address(cErc20DelegatorB));
+        CErc20delegateA = new CErc20Delegate();
+        console.log("CErc20delegateA", address(CErc20delegateA));
+        CErc20delegateB = new CErc20Delegate();
     }
 
     function deployUnitroller() public {
         // deploy unitroller and add configuration
         unitroller = new Unitroller();
         uniTrollerProxy = Comptroller(address(unitroller));
-        console.log("unitroller-proxy", address(uniTrollerProxy));
+        // console.log("unitroller-proxy", address(unitrollerproxy));
         unitroller._setPendingImplementation(address(comptroller));
         comptroller._become(unitroller);
+        // console.log("unitroller", address(unitroller));
+    }
 
-        console.log("unitroller", address(unitroller));
+    function deployComptroller() public {
+        // deply cerc20delegator
+        //(    address underlying_,
+        //     comptrollerinterface comptroller_,
+        //     InterestRateModel interestratemodel_,
+        //     uint initialexchangeratemantissa_,
+        //     string memory name_,
+        //     string memory symbol_,
+        //     uint8 decimals_,
+        //     address payable admin_,
+        //     address implementation_ )
+        console.log("USDC", address(USDC));
+        CErc20DelegatorUSDC = new CErc20Delegator(
+            address(USDC),
+            Comptroller(address(unitroller)),
+            InterestRateModel(rateModel),
+            1e6,
+            "ctoken usdc",
+            "ctokenusdc",
+            18,
+            payable(address(this)),
+            address(CErc20delegateA),
+            "0x0"
+        );
+        CErc20DelegatorUNI = new CErc20Delegator(
+            address(UNI),
+            Comptroller(address(unitroller)),
+            InterestRateModel(rateModel),
+            1e18,
+            "ctoken uni",
+            "ctokenuni",
+            18,
+            payable(address(this)),
+            address(CErc20delegateB),
+            "0x0"
+        );
     }
 
     function postDeploy() public {
         simpleOracle = new SimplePriceOracle();
 
         uniTrollerProxy._setPriceOracle(simpleOracle);
+        // simpleOracle.setDirectPrice(address(USDC), 1);
+        // simpleOracle.setDirectPrice(address(UNI), 5);
         simpleOracle.setUnderlyingPrice(
-            CToken(address(cErc20DelegatorA)),
-            1e18
+            CToken(address(CErc20DelegatorUSDC)),
+            1e30
         );
         simpleOracle.setUnderlyingPrice(
-            CToken(address(cErc20DelegatorB)),
-            100e18
+            CToken(address(CErc20DelegatorUNI)),
+            5 * 1e18
         );
 
-        uniTrollerProxy._setLiquidationIncentive(1e18);
-        uniTrollerProxy._supportMarket(CToken(address(cErc20DelegatorA)));
-        uniTrollerProxy._supportMarket(CToken(address(cErc20DelegatorB)));
+        uniTrollerProxy._setLiquidationIncentive(1.08 * 1e18);
+        uniTrollerProxy._supportMarket(CToken(address(CErc20DelegatorUSDC)));
+        uniTrollerProxy._supportMarket(CToken(address(CErc20DelegatorUNI)));
         uniTrollerProxy._setCloseFactor(.5 * 1e18);
         uniTrollerProxy._setCollateralFactor(
-            CToken(address(cErc20DelegatorB)),
+            CToken(address(CErc20DelegatorUNI)),
             .5 * 1e18
         );
     }
--- section3/CompoundPractice/test/CompoundLiquidationAAVE.t.sol
+++ section3/CompoundPractice/test/CompoundLiquidationAAVE.t.sol
@@ -1,39 +1,69 @@
 import "forge-std/console.sol";
 import "../src/CERC20Setup.sol";
+import {Test} from "forge-std/Test.sol";
 
-// import {Comptroller} from "compound-protocol/contracts/Comptroller.sol";
+// import {IFlashLoanSimpleReceiver, IPoolAddressesProvider, IPool} from "aave-v3-core/contracts/flashloan/interfaces/IFlashLoanSimpleReceiver.sol";
 
-contract CompoundLiquidationAAVE is CERC20Setup {
+contract CompoundLiquidationAAVE is CERC20Setup, Test {
     address user1;
     address user2;
     address admin;
     uint mainnetFork;
+    uint USDCDecimals = 6;
+    uint UNIDecmials = 18;
+    address public constant UNIAddress =
+        0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
+    address public constant USDCAddress =
+        0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     string constant MAINNET_RPC_URL = "https://eth.llamarpc.com";
 
     function setUp() public {
-        setUpcErc20();
         vm.createSelectFork(MAINNET_RPC_URL, 17465000);
+        setUpcErc20();
         console.log("address in CERC20Test", address(this));
 
         user1 = makeAddr("user1");
         user2 = makeAddr("user2");
 
-        vm.startPrank(user2);
-        underlyingTokenA.mint(150 * 10 ** underlyingTokenA.decimals());
-        vm.stopPrank();
+        // give liquidity
+        deal(USDCAddress, address(this), 2500 * 10 ** USDCDecimals);
+        USDC.approve(address(CErc20DelegatorUSDC), 2500 * 10 ** USDCDecimals);
+        CErc20DelegatorUSDC.mint(2500 * 10 ** USDCDecimals);
 
+        console.log(
+            "usdcBalance",
+            USDC.balanceOf(address(CErc20DelegatorUSDC))
+        );
+
+        deal(UNIAddress, address(user1), 1000 * 10 ** UNIDecmials);
         vm.startPrank(user1);
-        underlyingTokenA.mint(100 * 10 ** underlyingTokenA.decimals());
-        underlyingTokenB.mint(1 * 10 ** underlyingTokenB.decimals());
-
-        address[] memory cTokens = new address[](1);
-        cTokens[0] = address(cErc20DelegatorB);
+        UNI.approve(address(CErc20DelegatorUNI), 1000 * 10 ** UNIDecmials);
+        CErc20DelegatorUNI.mint(1000 * 10 ** UNIDecmials);
+        address[] memory cTokens = new address[](2);
+        cTokens[0] = address(CErc20DelegatorUNI);
+        cTokens[1] = address(CErc20DelegatorUSDC);
         uint[] memory value = uniTrollerProxy.enterMarkets(cTokens);
-
+        // (, uint accountToken, , ) = CErc20DelegatorUNI.getAccountSnapshot(
+        //     address(user1)
+        // );
+        // console.log("accountToken user1", accountToken);
+        CErc20DelegatorUSDC.borrow(2500 * 10 ** USDCDecimals);
         vm.stopPrank();
     }
 
-    function test_aave_liquidation() public {
-        vm.rollFork(15815693);
+    function executeOperation(
+        address assets,
+        uint256 amounts,
+        uint256 premiums,
+        address initiator,
+        bytes calldata params
+    ) external returns (bool) {
+        // address checkerAddress = abi.decode(params, (address));
+        // BalanceChecker checker = BalanceChecker(checkerAddress);
+        // checker.checkBalance();
+        // IERC20(USDC).approve(address(POOL()), premiums + amounts);
+        // return true;
     }
+
+    function test_aave_liquidation() public {}
 }
